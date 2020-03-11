775314fac3144cb3a684013792e98f40837d3096
Truong Pham <pntruong1@tma.com.vn>
2020-03-11 10:34:38 +0700

    Updates

---
+++ precache-manifest.d7380afd5041c644358328926c9befbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44bce434d563b041d369f1ec300a6bce",
+    "url": "/portlofio/index.html"
+  },
+  {
+    "revision": "201b1d42e532405da459",
+    "url": "/portlofio/static/css/main.35acba21.chunk.css"
+  },
+  {
+    "revision": "154ec3bfd29d50e1f773",
+    "url": "/portlofio/static/js/2.30dde2ab.chunk.js"
+  },
+  {
+    "revision": "cfae5b7d83e903e3eac5d300e4eb8849",
+    "url": "/portlofio/static/js/2.30dde2ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201b1d42e532405da459",
+    "url": "/portlofio/static/js/main.861274f4.chunk.js"
+  },
+  {
+    "revision": "7554bb0ca0fd07b8508d",
+    "url": "/portlofio/static/js/runtime-main.82d51551.js"
+  }
+]);
